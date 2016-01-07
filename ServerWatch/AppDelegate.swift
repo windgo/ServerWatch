19fbddcab392d4743f1bfc2f757e6b26e7ee2c8a
windgo <imqhf@hotmail.com>
2016-01-07 12:08:23 +0800

    Initial Commit

---
+++ ServerWatch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ServerWatch
+//
+//  Created by qi on 16/1/7.
+//  Copyright © 2016年 im.windgo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ServerWatch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ServerWatch
+//
+//  Created by qi on 16/1/7.
+//  Copyright © 2016年 im.windgo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
